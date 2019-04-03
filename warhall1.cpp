4487083bc1345f673b14d02b40f123a122e809e3
IustinPostolache <35453527+IustinPostolache@users.noreply.github.com>
2019-04-03 09:46:22 +0300

    Add files via upload

---
+++ warhall1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"0 1 1 1 1"<<endl;
+
+    cout<<"0 0 1 1 1"<<endl;
+
+    cout<<"0 0 0 0 0"<<endl;
+
+    cout<<"0 0 1 0 1"<<endl;
+
+    cout<<"0 0 1 0 0"<<endl;
+}
